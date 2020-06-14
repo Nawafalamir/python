eb721db3a1bd023985f89a1aa95191f6c411bccc
Nawafalamir <66888415+Nawafalamir@users.noreply.github.com>
2020-06-14 19:33:56 +0300

    firrst lesson

--- hello.py
+++ hello.py
@@ -5,7 +5,17 @@ print(123)
 print(87)
 print("last")
 print("first")
-
+print("\n")# print new space line betwen the two code
 print("rds")
 print("drsa")
-print(3484)
+print('3484 \n')# print new line space after this code 
+print('nasd')
+
+print('''this is multi line code
+so we need to do this ''' )# print multi line
+print(''' we need
+to do this 
+as far as quilcly
+at 3 pm ''')
+#multinecode with four line with usin ''' 
+
